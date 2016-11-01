1ff830e0816bfd954155b44154ba6fafac4c99f3
Bu88le <joseffeger@gmx.de>
2016-11-01 22:00:25 +0100

    AKonjugation in Auswahl eingebaut

--- Latein/src/main/java/logic/Auswahl.java
+++ Latein/src/main/java/logic/Auswahl.java
@@ -1,8 +1,15 @@
 package logic;
 
+import java.util.ArrayList;
+
+import translation.AKonVocs;
+
 public class Auswahl {
 
-	String gvs;
+	String gvs, info;
+	boolean aKon, eKon, iKon, konsKon;
+	int vocCount;
+	ArrayList<String> foundVoc = new ArrayList<String>();
 
 
 	public Auswahl(String gvs) {
@@ -14,6 +21,24 @@ public class Auswahl {
 
 
 	private void testing() {
+
+		for (int i = 0; i < AKonVocs.getAKonStamm().size(); i++) {
+			for (int z = 0; z < AKonVocs.getAKonStamm().get(i).length; z++) {
+				if (gvs.equals(AKonVocs.getAKonStamm().get(i)[z])) {
+					aKon = true;
+					vocCount++;
+					if (i == 4) {
+						info = "Deponens";
+						foundVoc.add(AKonVocs.getAKonStamm().get(i)[z]);
+					} else if (i == 2) {
+						info = "u-Perfekt";
+						foundVoc.add(AKonVocs.getAKonStamm().get(3)[z]);
+					} else {
+						foundVoc.add(AKonVocs.getAKonStamm().get(i)[z]);
+					}
+				}
+			}
+		}
 
 	}
 
