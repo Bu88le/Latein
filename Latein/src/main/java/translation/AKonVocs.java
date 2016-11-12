878382f1275be2c6f5bc78cad79f7cd1c888f496
WilliGross <dergrossewilligross@gmail.com>
2016-11-12 18:33:41 +0100

    Use InputStreams to read vocabulary files

--- Latein/src/main/java/translation/AKonVocs.java
+++ Latein/src/main/java/translation/AKonVocs.java
@@ -2,10 +2,9 @@ package translation;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
@@ -14,18 +13,19 @@ import bu88le.gui.MainFrame;
 
 public class AKonVocs {
 
-	static ArrayList<String[]> gesamtVokabeln = new ArrayList<String[]>();
-	private static ArrayList<String[]> stammVocs = new ArrayList<String[]>();
-	private ArrayList<String> foundVoc = new ArrayList<String>();
-	private StringBuffer ausgabe = new StringBuffer("<html>");
+	static ArrayList<String[]>			gesamtVokabeln	= new ArrayList<>();
+	private static ArrayList<String[]>	stammVocs		= new ArrayList<>();
+	private ArrayList<String>			foundVoc		= new ArrayList<>();
+	private final StringBuffer			ausgabe			= new StringBuffer("<html>");
 
-	String auslesen;
-	URL url1 = getClass().getClassLoader().getResource("vocabulary/akon.dat");
-	URL url2 = getClass().getClassLoader().getResource("stems/akon.dat");
+	String								auslesen;
+	InputStream							inputStream1	= getClass().getClassLoader()
+			.getResourceAsStream("vocabulary/akon.dat");
+	InputStream							inputStream2	= getClass().getClassLoader()
+			.getResourceAsStream("stems/akon.dat");
 
-	File file = null;
-	BufferedReader reader = null;
-
+	File								file			= null;
+	BufferedReader						reader			= null;
 
 	public AKonVocs() {
 		gesamtVokabeln.clear();
@@ -34,7 +34,6 @@ public class AKonVocs {
 		vocsAuslesen();
 	}
 
-
 	public AKonVocs(ArrayList<String> foundVoc) {
 		this.foundVoc.clear();
 		this.foundVoc = foundVoc;
@@ -42,108 +41,82 @@ public class AKonVocs {
 		ausgabe();
 	}
 
-
 	private void ausgabe() {
 
-		for (int i = 0; i < gesamtVokabeln.size(); i++) {
+		for (int i = 0; i < gesamtVokabeln.size(); i++)
 			if (gesamtVokabeln.get(i)[0].contains(foundVoc.get(0))) {
-				for (int z = 1; z < gesamtVokabeln.get(i).length; z++) {
+				for (int z = 1; z < gesamtVokabeln.get(i).length; z++)
 					ausgabe.append(gesamtVokabeln.get(i)[z]);
-				}
 				break;
 			}
-		}
 
 		ausgabe.append("<html/>");
 		MainFrame.panelTranslation(ausgabe);
 	}
 
-
 	public void vocsAuslesen() {
-		try {
-			file = new File(url1.toURI());
-			reader = new BufferedReader(new FileReader(file));
-		} catch (FileNotFoundException fnfe) {
-			JOptionPane.showMessageDialog(null, "Datei nicht gefunden. Bitte umgehend an den Support wenden!",
-					"Warning: File not found", JOptionPane.WARNING_MESSAGE);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+		reader = new BufferedReader(new InputStreamReader(inputStream1));
+		
 		try {
 			auslesen = reader.readLine();
-			while (auslesen != null) {
+			while (auslesen != null)
 				try {
 					gesamtVokabeln.add(auslesen.split(","));
+					//					for (final String[] strings : gesamtVokabeln)
+					//						for (final String str : strings)
+					//							System.out.println(str);
 					auslesen = reader.readLine();
-				} catch (IOException e) {
+				} catch (final IOException e) {
 					// TODO Auto-generated catch block
 					JOptionPane.showMessageDialog(null, e, "", JOptionPane.INFORMATION_MESSAGE);
 				}
-
-			}
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			if (reader != null) {
+			if (reader != null)
 				try {
 					reader.close();
-				} catch (IOException e) {
+				} catch (final IOException e) {
 					// TODO Auto-generated catch block
 					JOptionPane.showMessageDialog(null, e, "", JOptionPane.INFORMATION_MESSAGE);
 				}
-			}
-
+			
 		}
 	}
 
-
 	public void stammAuslesen() {
-		try {
-			file = new File(url2.toURI());
-			reader = new BufferedReader(new FileReader(file));
-		} catch (FileNotFoundException fnfe) {
-			JOptionPane.showMessageDialog(null, "Datei nicht gefunden. Bitte umgehend an den Support wenden!",
-					"Warning: File not found", JOptionPane.WARNING_MESSAGE);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 
+		reader = new BufferedReader(new InputStreamReader(inputStream2));
 		try {
 			auslesen = reader.readLine();
-			while (auslesen != null) {
+			while (auslesen != null)
 				try {
 					stammVocs.add(auslesen.split(","));
 					auslesen = reader.readLine();
-				} catch (IOException e) {
+				} catch (final IOException e) {
 					// TODO Auto-generated catch block
 					JOptionPane.showMessageDialog(null, e, "", JOptionPane.INFORMATION_MESSAGE);
 				}
-
-			}
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			if (reader != null) {
+			if (reader != null)
 				try {
 					reader.close();
-				} catch (IOException e) {
+				} catch (final IOException e) {
 					// TODO Auto-generated catch block
 					JOptionPane.showMessageDialog(null, e, "", JOptionPane.INFORMATION_MESSAGE);
 				}
-			}
 
 		}
 
 	}
-
 
 	public static ArrayList<String[]> getAKonStamm() {
 		return stammVocs;
 	}
-
 
 	public static ArrayList<String[]> getAKonVocs() {
 		return gesamtVokabeln;
--- Latein/src/main/java/wordShaping/AKonjugation.java
+++ Latein/src/main/java/wordShaping/AKonjugation.java
@@ -1,293 +1,245 @@
 package wordShaping;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class AKonjugation extends Thread {
-
-	BufferedReader reader = null;
-	URL url1 = this.getClass().getClassLoader()
-			.getResource("endings" + File.separator + "aKon" + File.separator + "normalAKon.dat");
-	String gvs;
-
-	static String[] präsensAktivIndikativ = new String[6];
-	static String[] präsensAktivKonjunktiv = new String[6];
-	static String[] präsensPassivIndikativ = new String[6];
-	static String[] präsensPassivKonjunktiv = new String[6];
-
-	static String[] imperfektAktivIndikativ = new String[6];
-	static String[] imperfektAktivKonjunktiv = new String[6];
-	static String[] imperfektPassivIndikativ = new String[6];
-	static String[] imperfektPassivKonjunktiv = new String[6];
-
-	static String[] futurIAktivIndikativ = new String[6];
-	static String[] futurIPassivIndikativ = new String[6];
-
-	static String[] perfektAktivIndikativ = new String[6];
-	static String[] perfektAktivKonjunktiv = new String[6];
-	static String[] perfektPassivIndikativ = new String[6];
-	static String[] perfektPassivKonjunktiv = new String[6];
-
-	static String[] plusquamperfektAktivIndikativ = new String[6];
-	static String[] plusquamperfektAktivKonjunktiv = new String[6];
-	static String[] plusquamperfektPassivIndikativ = new String[6];
-	static String[] plusquamperfektPassivKonjunktiv = new String[6];
-
-	static String[] futurIIAktivIndikativ = new String[6];
-	static String[] futurIIPassivKonjunktiv = new String[6];
-
-
+	
+	BufferedReader	reader							= null;
+	InputStream		inputStream						= getClass().getClassLoader()
+			.getResourceAsStream("endings/aKon/normalAKon.dat");
+	String			gvs;
+	
+	static String[]	präsensAktivIndikativ			= new String[6];
+	static String[]	präsensAktivKonjunktiv			= new String[6];
+	static String[]	präsensPassivIndikativ			= new String[6];
+	static String[]	präsensPassivKonjunktiv			= new String[6];
+	
+	static String[]	imperfektAktivIndikativ			= new String[6];
+	static String[]	imperfektAktivKonjunktiv		= new String[6];
+	static String[]	imperfektPassivIndikativ		= new String[6];
+	static String[]	imperfektPassivKonjunktiv		= new String[6];
+	
+	static String[]	futurIAktivIndikativ			= new String[6];
+	static String[]	futurIPassivIndikativ			= new String[6];
+	
+	static String[]	perfektAktivIndikativ			= new String[6];
+	static String[]	perfektAktivKonjunktiv			= new String[6];
+	static String[]	perfektPassivIndikativ			= new String[6];
+	static String[]	perfektPassivKonjunktiv			= new String[6];
+	
+	static String[]	plusquamperfektAktivIndikativ	= new String[6];
+	static String[]	plusquamperfektAktivKonjunktiv	= new String[6];
+	static String[]	plusquamperfektPassivIndikativ	= new String[6];
+	static String[]	plusquamperfektPassivKonjunktiv	= new String[6];
+	
+	static String[]	futurIIAktivIndikativ			= new String[6];
+	static String[]	futurIIPassivKonjunktiv			= new String[6];
+	
 	public AKonjugation(String gvs) {
 		this.gvs = gvs;
 	}
-
-
+	
+	@Override
 	public void run() {
 		aKonNormal();
 	}
-
-
+	
 	private void aKonNormal() {
 		try {
-			File file = new File(url1.toURI());
-			reader = new BufferedReader(new FileReader(file));
+			reader = new BufferedReader(new InputStreamReader(inputStream));
 			String auslesen = reader.readLine();
-
-			ArrayList<String[]> auslesenArray = new ArrayList<>();
+			
+			final ArrayList<String[]> auslesenArray = new ArrayList<>();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(0).length; i++) {
+			for (int i = 0; i < auslesenArray.get(0).length; i++)
 				präsensAktivIndikativ[i] = gvs + auslesenArray.get(0)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(1).length; i++) {
+			for (int i = 0; i < auslesenArray.get(1).length; i++)
 				präsensAktivKonjunktiv[i] = gvs + auslesenArray.get(1)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(2).length; i++) {
+			for (int i = 0; i < auslesenArray.get(2).length; i++)
 				präsensPassivIndikativ[i] = gvs + auslesenArray.get(2)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(3).length; i++) {
+			for (int i = 0; i < auslesenArray.get(3).length; i++)
 				präsensPassivKonjunktiv[i] = gvs + auslesenArray.get(3)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(4).length; i++) {
+			for (int i = 0; i < auslesenArray.get(4).length; i++)
 				imperfektAktivIndikativ[i] = gvs + auslesenArray.get(4)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(5).length; i++) {
+			for (int i = 0; i < auslesenArray.get(5).length; i++)
 				imperfektAktivKonjunktiv[i] = gvs + auslesenArray.get(5)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(6).length; i++) {
+			for (int i = 0; i < auslesenArray.get(6).length; i++)
 				imperfektPassivIndikativ[i] = gvs + auslesenArray.get(6)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(7).length; i++) {
+			for (int i = 0; i < auslesenArray.get(7).length; i++)
 				imperfektPassivKonjunktiv[i] = gvs + auslesenArray.get(7)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(8).length; i++) {
+			for (int i = 0; i < auslesenArray.get(8).length; i++)
 				futurIAktivIndikativ[i] = gvs + auslesenArray.get(8)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(9).length; i++) {
+			for (int i = 0; i < auslesenArray.get(9).length; i++)
 				futurIPassivIndikativ[i] = gvs + auslesenArray.get(9)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(10).length; i++) {
+			for (int i = 0; i < auslesenArray.get(10).length; i++)
 				perfektAktivIndikativ[i] = gvs + auslesenArray.get(10)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(11).length; i++) {
+			for (int i = 0; i < auslesenArray.get(11).length; i++)
 				perfektAktivKonjunktiv[i] = gvs + auslesenArray.get(11)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(12).length; i++) {
+			for (int i = 0; i < auslesenArray.get(12).length; i++)
 				perfektPassivIndikativ[i] = gvs + auslesenArray.get(12)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(13).length; i++) {
+			for (int i = 0; i < auslesenArray.get(13).length; i++)
 				perfektPassivKonjunktiv[i] = gvs + auslesenArray.get(13)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(14).length; i++) {
+			for (int i = 0; i < auslesenArray.get(14).length; i++)
 				plusquamperfektAktivIndikativ[i] = gvs + auslesenArray.get(14)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(15).length; i++) {
+			for (int i = 0; i < auslesenArray.get(15).length; i++)
 				plusquamperfektAktivKonjunktiv[i] = gvs + auslesenArray.get(15)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(16).length; i++) {
+			for (int i = 0; i < auslesenArray.get(16).length; i++)
 				plusquamperfektPassivIndikativ[i] = gvs + auslesenArray.get(16)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(17).length; i++) {
+			for (int i = 0; i < auslesenArray.get(17).length; i++)
 				plusquamperfektPassivKonjunktiv[i] = gvs + auslesenArray.get(17)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(18).length; i++) {
+			for (int i = 0; i < auslesenArray.get(18).length; i++)
 				futurIIAktivIndikativ[i] = gvs + auslesenArray.get(18)[i];
-			}
-
+			
 			auslesen = reader.readLine();
 			auslesenArray.add(auslesen.split(","));
-			for (int i = 0; i < auslesenArray.get(19).length; i++) {
+			for (int i = 0; i < auslesenArray.get(19).length; i++)
 				futurIIPassivKonjunktiv[i] = gvs + auslesenArray.get(19)[i];
-			}
-
-		} catch (URISyntaxException e) {
+			
+		} catch (final FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
-
+	
 	public static String[] returnPräsensAktivIndikativ() {
 		return präsensAktivIndikativ;
-
+		
 	}
-
-
+	
 	public static String[] returnPräsensAktivKonjunktiv() {
 		return präsensAktivKonjunktiv;
 	}
-
-
+	
 	public static String[] returnPräsensPassivIndikativ() {
 		return präsensPassivIndikativ;
 	}
-
-
+	
 	public static String[] returnPräsensPassivKonjunktiv() {
 		return präsensPassivKonjunktiv;
 	}
-
-
+	
 	public static String[] returnImperfektAktivIndikativ() {
 		return imperfektAktivIndikativ;
 	}
-
-
+	
 	public static String[] returnImperfektAktvKonjunktiv() {
 		return imperfektAktivKonjunktiv;
 	}
-
-
+	
 	public static String[] returnImperfektPassivIndikativ() {
 		return imperfektPassivIndikativ;
 	}
-
-
+	
 	public static String[] returnImperfektPassivKonjunktiv() {
 		return imperfektPassivKonjunktiv;
 	}
-
-
+	
 	public static String[] returnFuturIAktivIndikativ() {
 		return futurIAktivIndikativ;
 	}
-
-
+	
 	public static String[] returnFuturIPassivIndikativ() {
 		return futurIPassivIndikativ;
 	}
-
-
+	
 	public static String[] returnPerfektAktivIndikativ() {
 		return perfektAktivIndikativ;
 	}
-
-
+	
 	public static String[] returnPerfektAktivKonjunktiv() {
 		return perfektAktivKonjunktiv;
 	}
-
-
+	
 	public static String[] returnPerfektPassivIndikativ() {
 		return perfektPassivIndikativ;
 	}
-
-
+	
 	public static String[] returnPerfektPassivKonjunktiv() {
 		return perfektPassivKonjunktiv;
 	}
-
-
+	
 	public static String[] returnPlusquamperfektAktivIndikativ() {
 		return plusquamperfektAktivIndikativ;
 	}
-
-
+	
 	public static String[] returnPlusquamperfektAktivKonjunktiv() {
 		return plusquamperfektAktivKonjunktiv;
 	}
-
-
+	
 	public static String[] returnPlusquamperfektPassivIndikativ() {
 		return plusquamperfektPassivIndikativ;
 	}
-
-
+	
 	public static String[] returnPlusquamperfektPassivKonjunktiv() {
 		return plusquamperfektPassivKonjunktiv;
 	}
-
-
+	
 	public static String[] returnFuturIIAktivIndikativ() {
 		return futurIIAktivIndikativ;
 	}
-
-
+	
 	public static String[] returnFuturIIPassivIndikativ() {
 		return futurIIPassivKonjunktiv;
 	}
